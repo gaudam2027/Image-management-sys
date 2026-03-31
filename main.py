799f83a9f4879e44a85f636e4fe51449f2bae3f3
gautham2027 <gaudam2027@gmail.com>
2026-04-01 02:09:57 +0530

    logging: added globl and separate log file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from config.db import Base, engine
 from routes import auth_routes,user_routes,image_routes,admin_routes
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
+from utils.logger import get_logger
 
 from models.user_model import User
 from models.image_model import Image
@@ -12,4 +15,29 @@ app.include_router(user_routes.router)
 app.include_router(image_routes.router)
 app.include_router(admin_routes.router)
 
+logger = get_logger("global")
+
 Base.metadata.create_all(bind=engine)
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    logger.warning(
+        f"HTTPException | {request.method} {request.url} | "
+        f"Status: {exc.status_code} | Detail: {exc.detail}"
+    )
+
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail},
+    )
+    
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    logger.exception(
+        f"Unhandled Error | Path: {request.url}"
+    )
+
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Internal Server Error"},
+    )
--- utils/logger.py
+++ utils/logger.py
@@ -6,16 +6,45 @@ LOG_DIR = "logs"
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
 
-LOG_FILE = os.path.join(LOG_DIR, "app.log")
+class LevelFilter(logging.Filter):
+    def __init__(self, level):
+        self.level = level
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
-    handlers=[
-        logging.FileHandler(LOG_FILE),
-        logging.StreamHandler()  # console
-    ]
+    def filter(self, record):
+        return record.levelno == self.level
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
 )
+
+info_handler = logging.FileHandler(os.path.join(LOG_DIR, "info.log"))
+info_handler.setLevel(logging.INFO)
+info_handler.addFilter(LevelFilter(logging.INFO))
+info_handler.setFormatter(formatter)
+
+warning_handler = logging.FileHandler(os.path.join(LOG_DIR, "warning.log"))
+warning_handler.setLevel(logging.WARNING)
+warning_handler.addFilter(LevelFilter(logging.WARNING))
+warning_handler.setFormatter(formatter)
+
+error_handler = logging.FileHandler(os.path.join(LOG_DIR, "error.log"))
+error_handler.setLevel(logging.ERROR)
+error_handler.addFilter(LevelFilter(logging.ERROR))
+error_handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(formatter)
+
+logger.addHandler(info_handler)
+logger.addHandler(warning_handler)
+logger.addHandler(error_handler)
+logger.addHandler(console_handler)
+
 
 def get_logger(name: str):
     return logging.getLogger(name)
